ce1f0848ee132b34a45a08e6e5294ed71b3cdb55
Bruno <mguimaraesbruno@gmail.com>
2022-02-09 23:39:16 -0300

    add font-face to global css index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { BrowserRouter } from "react-router-dom";
+import App from "./pages/App";
+
+import "./index.css";
+
+const apolloClient = new ApolloClient({
+  uri: process.env.REACT_APP_API_URL,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
---
+++ src/pages/App/index.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="App">
+      <span>
+        Topbar
+      </span>
+      <span>
+        Content
+      </span>
+    </main>
+  );
+}
+
+export default App;
